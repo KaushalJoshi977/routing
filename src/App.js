4b0fbf7974b6961613afd2241024b31b40701166
KaushalJoshi977 <118073045+KaushalJoshi977@users.noreply.github.com>
2024-03-12 11:19:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from './Component/Navbar'
+import About from './Component/About';
+import TV from './Component/TV';
+import Home from './Component/Home';
+import './Component/navbar.css';
+import './Component/Tv.css'
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/Home" element={<Home/>} />
+
+        <Route path="/About" element={<About/>} />
+
+        <Route path="/TV" element={<TV/>} />
+
+     </Routes>
+     <footer>
+     <div className="footer-content">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <ul className="footer-links">
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+     </footer>
+
+    </div>
+
+
+  );
+
+}
 export default App;
---
+++ src/Component/Home.js
@@ -0,0 +1,29 @@
+
+export default function Home(){
+    return(
+    <>
+<div className="home">
+<h2>Welcome to AnayaMart</h2>
+<p>
+        Discover a wide range of electronic items, including TVs, laptops, and mobile phones, at competitive prices. 
+        Shop with confidence and enjoy a seamless online shopping experience.
+</p>
+ 
+      <section className="product-categories">
+<div className="category">
+<h3>TVs</h3>
+<p>Explore our latest collection of high-definition TVs with stunning picture quality.</p>
+</div>
+<div className="category">
+<h3>Laptops</h3>
+<p>Find the perfect laptop for work, gaming, or entertainment in our diverse laptop selection.</p>
+</div>
+ 
+        <div className="category">
+<h3>Mobile Phones</h3>
+<p>Stay connected with the latest smartphones featuring cutting-edge technology and design.</p>
+</div>
+</section>
+</div>
+</>)
+    }
